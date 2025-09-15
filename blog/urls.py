7c5623f3ec7435eda807cd007d973e6ae8abe21a
kirill <kirill48108@gmail.com>
2025-09-15 13:21:17 +0300

    INIT

---
+++ blog/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+
+from .views import (
+    BlogPostListView,
+    BlogPostCreateView,
+    BlogPostDetailView,
+    BlogPostUpdateView,
+    BlogPostDeleteView
+)
+
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', BlogPostListView.as_view(), name='blog_list'),
+    path('create/', BlogPostCreateView.as_view(), name='blog_create'),
+    path('<int:pk>/', BlogPostDetailView.as_view(), name='blog_detail'),
+    path('<int:pk>/update/', BlogPostUpdateView.as_view(), name='blog_update'),
+    path('<int:pk>/delete/', BlogPostDeleteView.as_view(), name='blog_delete'),
+]
